41c338c2c42f6dc2154ee12faf4d309ef9570c01
ShenQianli <shenqianli@pku.edu.cn>
2017-05-06 23:11:25 +0800

    just add some lines for test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "prime_gen.h"
 #include "relation_collect.h"
 #include "large_integer.h"
+#include "linear_combiner.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 
 long f;	
@@ -13,8 +17,30 @@ int main(int argc, char const *argv[])
 	prime_gen();
 	//prime_print();
 
-	collect(f, 8, 8);
-	print_relation(8, 8);
-	
+	collect(f, 9, 8);
+	//print_relation(9, 8);
+    bool* t=linear_combiner(9, 8);
+    if(t==NULL){
+        printf("failure\n");
+        return 0;
+    }
+    print_combiniation(t, 9);
+    long A=1,B=1;
+    for(int i=0;i<D_R;++i){
+        if(t[i]){
+            A*=y[i];
+            B*=y[i]*y[i]-f;
+        }
+    }
+    B=(long)sqrt(double(B));
+    printf("A=%ld,B=%ld\n",A,B);
+    long gcd=mygcd(A-B,f);
+    if(gcd==1){
+        printf("failure\n");
+        return 0;
+    }
+    printf("success!%ld=%ld*%ld\n",f,gcd,f/gcd);
+    
+    free(t);
 	return 0;
 }
