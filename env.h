7001d9048b612fb6ba254f444f40a21288246ae5
ShenQianli <shenqianli@pku.edu.cn>
2017-05-06 23:04:42 +0800

    add an array r[] to store the remain num

--- env.h
+++ env.h
@@ -1,13 +1,14 @@
 #ifndef ENV_H
 #define ENV_H
 
-#define D_P 10000  // Dimension of primes
-#define D_R 10000	// Dimension of relations
+#define D_P 100  // Dimension of primes
+#define D_R 100	// Dimension of relations
 using namespace std;
 
 extern long p[D_P+10]; 			// Prime table
 extern bool m[D_R+10][D_P+10]; 	// Relation table
 extern long y[D_R+10];			// Y table for each relation (y[i]^2 is factored in m[i])
+extern long r[D_R+10];          // Remain number table of factor
 extern long f;					// Number to factor
 
 #endif
