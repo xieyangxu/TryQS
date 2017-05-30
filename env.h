1ad4093d4434fee750402c0513ac619108fef587
ShenQianli <shenqianli@pku.edu.cn>
2017-05-30 21:45:21 +0800

    add q[]

--- env.h
+++ env.h
@@ -10,6 +10,7 @@ extern long p[D_P+10]; 			// Prime table
 extern bool m[D_R+10][D_P+10]; 	// Relation table
 extern long y[D_R+10];			// Y table for each relation (y[i]^2 is factored in m[i])
 extern long r[D_R+10];          // Remain number table of factor
+extern long q[D_R+10];
 extern long f;					// Number to factor
 
 #endif
