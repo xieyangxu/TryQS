e682c6f2f90a4464d1db4b0f65390672a7f38400
ShenQianli <shenqianli@pku.edu.cn>
2017-05-31 18:46:39 +0800

    some parameter changed

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@
 
 
 
-BigInt f("991241");	
+BigInt f("2551097447021173");
 
 int main(int argc, char const *argv[])
 {
 	prime_gen();
 	//prime_print();
 
-	collect(f, 50, 30);
+    collect(f, D_R, D_P);
 	//void collect(long f, int num_relation, int num_prime);
-	print_relation(50, 30, 100);
+	//print_relation(D_R, D_P, 0);
 	//void print_relation(int num_relation, int num_prime, int num_partial);
 
-    myresolve(f,50,30,20);
+    myresolve(f,D_R,D_P,10000);
 	return 0;
 }
