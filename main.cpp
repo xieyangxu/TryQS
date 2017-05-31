6432238707343f250b8dcb19d11cc6e5e1986e95
ShenQianli <shenqianli@pku.edu.cn>
2017-06-01 02:58:32 +0800

    change the input way

--- main.cpp
+++ main.cpp
@@ -5,23 +5,33 @@
 #include "linear_combiner.h"
 #include "myresolve.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <math.h>
+time_t start_time;
+time_t cur_time;
 
 
-
-BigInt f("2551097447021173");
-
+BigInt f("0");
 int main(int argc, char const *argv[])
 {
-	prime_gen();
-	//prime_print();
+    int DR,DP;
+    string s;
+    s.resize(100);
+    prime_gen();
+    freopen("case.txt","r",stdin);
+    freopen("result.txt","w",stdout);
+    for(int i=0;i<24;++i){
+        cin>>s>>DR>>DP;
+        cout<<"case "<<i<<":"<<endl<<"f = "<<s<<" DR = "<<DR<<" DP = "<<DP<<endl;
+        f = BigInt(s.c_str());
+        //prime_print();
+        collect(f, DR, DP);
+        //void collect(long f, int num_relation, int num_prime);
+        //print_relation(D_R, D_P, 0);
+        //void print_relation(int num_relation, int num_prime, int num_partial);
 
-    collect(f, D_R, D_P);
-	//void collect(long f, int num_relation, int num_prime);
-	//print_relation(D_R, D_P, 0);
-	//void print_relation(int num_relation, int num_prime, int num_partial);
-
-    myresolve(f,D_R,D_P,10000);
+        myresolve(f,DR,DP,100);
+    }
 	return 0;
 }
